e31751281eac3ea51c36c7a47d9582f9fdff8b14
heyjoonkim <heyjoonkim@gmail.com>
2022-11-30 10:08:01 +0000

    update test batch size

--- scripts/clinc/cmu.sh
+++ scripts/clinc/cmu.sh
@@ -3,13 +3,11 @@
 export CUDA_VISIBLE_DEVICES=1
 
 
-lrs='1e-2 5e-3 1e-3 5e-4'
+lrs='1e-4 5e-5 1e-5 5e-6 1e-5'
 
 seeds='2134 3412 4132'
 
 
-lrs='1e-5'
-seeds='2134'
 
 for seed in  $seeds; do
     for lr in $lrs; do
