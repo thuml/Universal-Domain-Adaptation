9275a17f7c38d5a6d36015dccf5afad1f51dde47
heyjoonkim <heyjoonkim@gmail.com>
2022-12-21 08:19:33 +0000

    add scripts

---
+++ scripts/amazon/udanli_4_0.3.sh
@@ -0,0 +1,21 @@
+
+
+export CUDA_VISIBLE_DEVICES=0
+
+
+lrs='5e-5 1e-5 5e-6'
+
+seeds='1234 2134 3412 4132'
+
+
+
+
+lrs='5e-5 1e-5 5e-6'
+
+seeds='1234'
+
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/udanli_v9_cda.py --config configs/nlp/udanli-amazon-books-dvd_0.3.yaml --lr $lr --seed $seed --num_nli_sample 2
+    done
+done
