03c8ab9753335562990afc1f739b1a41f2bd70ce
heyjoonkim <heyjoonkim@gmail.com>
2022-12-21 14:20:24 +0900

    update configs

--- scripts/clinc/udanli_test.sh
+++ scripts/clinc/udanli_test.sh
@@ -4,7 +4,16 @@ export CUDA_VISIBLE_DEVICES=3
 
 
 lrs='1e-5'
-seeds='1234 2134 3412 4132'
+seeds='2134 4132'
+
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/udanli_v9_inference_only.py --config configs/nlp/udanli-clinc-opda_0.7.yaml --lr $lr --seed $seed --num_nli_sample 2
+    done
+done
+
+lrs='5e-5'
+seeds='1234 3412'
 
 for seed in  $seeds; do
     for lr in $lrs; do
--- scripts/massive/udanli_test.sh
+++ scripts/massive/udanli_test.sh
@@ -7,20 +7,20 @@ lrs='5e-5 1e-5 5e-6'
 
 seeds='1234 2134 3412 4132'
 
-seeds='1234 3412 4132'
+seeds='1234 4132'
 lrs='5e-6'
 
 for seed in  $seeds; do
     for lr in $lrs; do
-        python nlp/udanli_v9_inference_only.py --config configs/nlp/udanli-massive-opda_0.7.yaml --lr $lr --seed $seed --num_nli_sample 2
+        python nlp/udanli_v9_inference_only.py --config configs/nlp/udanli-massive-opda_0.8.yaml --lr $lr --seed $seed --num_nli_sample 2
     done
 done
 
-seeds='2134'
+seeds='2134 3412'
 lrs='1e-5'
 
 for seed in  $seeds; do
     for lr in $lrs; do
-        python nlp/udanli_v9_inference_only.py --config configs/nlp/udanli-massive-opda_0.7.yaml --lr $lr --seed $seed --num_nli_sample 2
+        python nlp/udanli_v9_inference_only.py --config configs/nlp/udanli-massive-opda_0.8.yaml --lr $lr --seed $seed --num_nli_sample 2
     done
 done
