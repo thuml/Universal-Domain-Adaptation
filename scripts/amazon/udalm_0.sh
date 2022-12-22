473e220d3d8aa1d1b5dde30cadcc56b4a40024fa
heyjoonkim <heyjoonkim@gmail.com>
2022-12-22 14:41:22 +0900

    update udalm

---
+++ scripts/amazon/udalm_0.sh
@@ -0,0 +1,19 @@
+
+
+export CUDA_VISIBLE_DEVICES=0
+
+
+seeds='1234 2134 3412 4132'
+
+
+lrs='5e-5 1e-5 6e-6'
+
+source='books'
+target='dvd'
+
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/udalm.py --config configs/nlp/udalm-amazon-$source-$target.yaml --lr $lr --seed $seed --batch_size 8
+    done
+done
+
