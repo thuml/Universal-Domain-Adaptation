172f1fba98787c52fec0a1366133e008ad4fdf4b
heyjoonkim <heyjoonkim@gmail.com>
2022-12-22 15:17:17 +0000

    update code

---
+++ scripts/amazon/uan.sh
@@ -0,0 +1,31 @@
+
+
+export CUDA_VISIBLE_DEVICES=2
+
+
+lrs='1e-5 5e-6 1e-6'
+
+seeds='1234 2134 3412 4132'
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-books-dvd.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-books-electronics.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-books-kitchen.yaml --lr $lr --seed $seed
+    done
+done
