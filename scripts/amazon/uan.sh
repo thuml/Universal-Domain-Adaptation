f294663d7d2b4a3018b42f7b74aa9a7ba916fbb4
heyjoonkim <heyjoonkim@gmail.com>
2022-12-25 22:05:46 +0900

    update amazon

--- scripts/amazon/uan.sh
+++ scripts/amazon/uan.sh
@@ -3,15 +3,40 @@
 export CUDA_VISIBLE_DEVICES=1
 
 
-lrs='1e-5 5e-6 1e-6'
-lrs='1e-3 5e-4 1e-4'
+lrs='1e-5 5e-6 1e-6 5e-7'
 
 seeds='1234 2134 3412 4132'
 
+# # B -> D, E, K
+# # CDA
+# for seed in  $seeds; do
+#     for lr in $lrs; do
+#         python nlp/uan.py --config configs/nlp/uan-amazon-books-dvd.yaml --lr $lr --seed $seed
+#     done
+# done
+
+
+# # CDA
+# for seed in  $seeds; do
+#     for lr in $lrs; do
+#         python nlp/uan.py --config configs/nlp/uan-amazon-books-electronics.yaml --lr $lr --seed $seed
+#     done
+# done
+
+
+# # CDA
+# for seed in  $seeds; do
+#     for lr in $lrs; do
+#         python nlp/uan.py --config configs/nlp/uan-amazon-books-kitchen.yaml --lr $lr --seed $seed
+#     done
+# done
+
+
+# D -> B, E, K
 # CDA
 for seed in  $seeds; do
     for lr in $lrs; do
-        python nlp/uan.py --config configs/nlp/uan-amazon-books-dvd.yaml --lr $lr --seed $seed
+        python nlp/uan.py --config configs/nlp/uan-amazon-dvd-books.yaml --lr $lr --seed $seed
     done
 done
 
@@ -19,7 +44,7 @@ done
 # CDA
 for seed in  $seeds; do
     for lr in $lrs; do
-        python nlp/uan.py --config configs/nlp/uan-amazon-books-electronics.yaml --lr $lr --seed $seed
+        python nlp/uan.py --config configs/nlp/uan-amazon-dvd-electronics.yaml --lr $lr --seed $seed
     done
 done
 
@@ -27,6 +52,56 @@ done
 # CDA
 for seed in  $seeds; do
     for lr in $lrs; do
-        python nlp/uan.py --config configs/nlp/uan-amazon-books-kitchen.yaml --lr $lr --seed $seed
+        python nlp/uan.py --config configs/nlp/uan-amazon-dvd-kitchen.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# E -> B, D, K
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-electronics-books.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-electronics-dvd.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-electronics-kitchen.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# K -> B, D, E
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-kitchen-books.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-kitchen-dvd.yaml --lr $lr --seed $seed
+    done
+done
+
+
+# CDA
+for seed in  $seeds; do
+    for lr in $lrs; do
+        python nlp/uan.py --config configs/nlp/uan-amazon-kitchen-electronics.yaml --lr $lr --seed $seed
     done
 done
