720a55495db405db45587cbeffe36544d2d8653f
heyjoonkim <heyjoonkim@gmail.com>
2023-01-08 21:22:09 +0900

    add ablations

--- scripts/massive/udalm_mlm_ablation.sh
+++ scripts/massive/udalm_mlm_ablation.sh
@@ -1,6 +1,6 @@
 
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 
 
 seeds='1234 2134 3412'
@@ -12,13 +12,37 @@ for seed in  $seeds; do
     python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-2.yaml --lr $lr --seed $seed
 done
 
+# common class = 4
+for seed in  $seeds; do
+    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-4.yaml --lr $lr --seed $seed
+done
+
 # common class = 6
 for seed in  $seeds; do
     python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-6.yaml --lr $lr --seed $seed
 done
 
-# common class = 8
+# common class = 10
 for seed in  $seeds; do
-    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-8.yaml --lr $lr --seed $seed
+    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-10.yaml --lr $lr --seed $seed
 done
+
+# common class = 12
+for seed in  $seeds; do
+    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-12.yaml --lr $lr --seed $seed
+done
+
+# common class = 14
+for seed in  $seeds; do
+    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-14.yaml --lr $lr --seed $seed
+done
+
+# common class = 16
+for seed in  $seeds; do
+    python nlp/udalm_mlm.py --config configs/nlp/ablation/udalm-massive-16.yaml --lr $lr --seed $seed
+done
+
+
+
+
 
